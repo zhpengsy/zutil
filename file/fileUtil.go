35e1f597dac66911695d0bf62a746dc0bc406ce5
张鹏 <981991557@qq.com>
2024-10-12 09:26:24 +0800

    文件工具类

---
+++ file/fileUtil.go
@@ -0,0 +1,91 @@
+package file
+
+import (
+	"archive/zip"
+	"bufio"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// 读取文本文件的每一行
+func readLines(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+// 复制文件
+func copyFile(src, dest string) error {
+	inputFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer inputFile.Close()
+
+	// 创建目标目录
+	destDir := filepath.Dir(dest)
+	if err := os.MkdirAll(destDir, 0755); err != nil {
+		return err
+	}
+
+	outputFile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+
+	_, err = io.Copy(outputFile, inputFile)
+	return err
+}
+
+// 压缩目录
+func zipDir(srcDir, zipFilePath string) error {
+	zipFile, err := os.Create(zipFilePath)
+	if err != nil {
+		return err
+	}
+	defer zipFile.Close()
+
+	zipWriter := zip.NewWriter(zipFile)
+	defer zipWriter.Close()
+
+	return filepath.Walk(srcDir, func(filePath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		relPath, err := filepath.Rel(srcDir, filePath)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			_, err = zipWriter.Create(relPath + "/")
+			return err
+		}
+
+		zipFileWriter, err := zipWriter.Create(relPath)
+		if err != nil {
+			return err
+		}
+
+		file, err := os.Open(filePath)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		_, err = io.Copy(zipFileWriter, file)
+		return err
+	})
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module zutil
+
+go 1.23.2
